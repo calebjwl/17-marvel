a94afc6dd41488367e636d80ccb17fdeab6e6082
Caleb Lee <caleb.gwl@gmail.com>
2017-03-06 21:14:00 -0600

    "Add more actions to actions.js"

--- app/actions.js
+++ app/actions.js
@@ -1,18 +1,18 @@
-export function loadSeriesInfo(data) {
+export function seriesInfoLoadComplete(data) {
   return {
     type: 'SERIES_INFO@LOAD_COMPLETE',
     data,
   };
 }
 
-export function findAllCharacters(data) {
+export function charactersLoadComplete(data) {
   return {
     type: 'CHARACTERS@FIND_ALL_COMPLETE',
     data,
   };
 }
 
-export function findAllComics(data) {
+export function comicsLoadComplete(data) {
   return {
     type: 'COMICS@FIND_ALL_COMPLETE',
     data,
@@ -30,4 +30,32 @@ export function clearModal() {
   return {
     type: 'MODAL@CLEAR',
   };
+}
+
+export function seriesInfoSearch(name) {
+  return (next) => {
+    fetch(`http://marvel-is-broke.herokuapp.com/series?limit=1&titleStartsWith=${name}`)
+    .then(response => response.json())
+    .then((data) => {
+      const id = next(seriesInfoLoadComplete(data));
+      next(charactersFindForId(id));
+      next(comicsFindForId(id));
+    });
+  };
+}
+
+export function charactersFindForId(id) {
+  fetch(`http://marvel-is-broke.herokuapp.com/series/${id}/characters`)
+  .then(response => response.json())
+  .then((data) => {
+    next(charactersLoadComplete(data));
+  });
+}
+
+export function comicsFindForId(id) {
+  fetch('http://marvel-is-broke.herokuapp.com/series/1928/comics')
+  .then(response => response.json())
+  .then((data) => {
+    next(comicsLoadComplete(data));
+  });
 }
--- app/components/application.vue
+++ app/components/application.vue
@@ -103,6 +103,7 @@
 </template>
 
 <script>
+import store from 'store';
 export default {
   data() {
     return {
